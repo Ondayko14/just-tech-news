51dc819cb47335a334ba3640c209b982f84f37d0
Brandon O <b.ondayko@gmail.com>
2021-01-12 17:14:43 -0600

    passwords are being hashed on updates and Creations

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const sequelize = require('../config/connection');
 const {Model, DataTypes} = require('sequelize');
+const bcrypt = require('bcrypt');
 
 //Create our user Module
 class User extends Model {}
@@ -39,6 +40,16 @@ User.init(
         }
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
     //Table Configuration Goes Here
       sequelize,
       timestamps: false,
